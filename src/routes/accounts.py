accbf7735b7cd0da619fca0905df66507fa742a5
Aleksey Pastukhov <mindflux99@gmail.com>
2025-03-18 14:55:28 +0200

    Solution 2

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -69,6 +69,7 @@ async def register_user(
         user_data: UserRegistrationRequestSchema,
         db: AsyncSession = Depends(get_db),
         email_sender: EmailSenderInterface = Depends(get_accounts_email_notificator),
+        background_tasks: BackgroundTasks = Depends(),
 ) -> UserRegistrationResponseSchema:
     """
     Endpoint for user registration.
@@ -81,6 +82,7 @@ async def register_user(
         user_data (UserRegistrationRequestSchema): The registration details including email and password.
         db (AsyncSession): The asynchronous database session.
         email_sender (EmailSenderInterface): The asynchronous email sender.
+        background_tasks: BackgroundTasks
 
     Returns:
         UserRegistrationResponseSchema: The newly created user's details.
@@ -131,7 +133,8 @@ async def register_user(
     else:
         activation_link = "http://127.0.0.1/accounts/activate/"
 
-        await email_sender.send_activation_email(
+        background_tasks.add_task(
+            email_sender.send_activation_email,
             new_user.email,
             activation_link
         )
@@ -174,6 +177,7 @@ async def activate_account(
         activation_data: UserActivationRequestSchema,
         db: AsyncSession = Depends(get_db),
         email_sender: EmailSenderInterface = Depends(get_accounts_email_notificator),
+        background_tasks: BackgroundTasks = Depends(),
 ) -> MessageResponseSchema:
     """
     Endpoint to activate a user's account.
@@ -187,6 +191,7 @@ async def activate_account(
         activation_data (UserActivationRequestSchema): Contains the user's email and activation token.
         db (AsyncSession): The asynchronous database session.
         email_sender (EmailSenderInterface): The asynchronous email sender.
+        background_tasks: BackgroundTasks
 
     Returns:
         MessageResponseSchema: A response message confirming successful activation.
@@ -231,7 +236,8 @@ async def activate_account(
 
     login_link = "http://127.0.0.1/accounts/login/"
 
-    await email_sender.send_activation_complete_email(
+    background_tasks.add_task(
+        email_sender.send_activation_complete_email,
         str(activation_data.email),
         login_link
     )
@@ -252,7 +258,8 @@ async def activate_account(
 async def request_password_reset_token(
         data: PasswordResetRequestSchema,
         db: AsyncSession = Depends(get_db),
-        email_sender: EmailSenderInterface = Depends(get_accounts_email_notificator)
+        email_sender: EmailSenderInterface = Depends(get_accounts_email_notificator),
+        background_tasks: BackgroundTasks = Depends(),
 ) -> MessageResponseSchema:
     """
     Endpoint to request a password reset token.
@@ -264,6 +271,7 @@ async def request_password_reset_token(
         data (PasswordResetRequestSchema): The request data containing the user's email.
         db (AsyncSession): The asynchronous database session.
         email_sender (EmailSenderInterface): The asynchronous email sender.
+        background_tasks: BackgroundTasks
 
     Returns:
         MessageResponseSchema: A success message indicating that instructions will be sent.
@@ -285,7 +293,8 @@ async def request_password_reset_token(
 
     password_reset_complete_link = "http://127.0.0.1/accounts/password-reset-complete/"
 
-    await email_sender.send_password_reset_email(
+    background_tasks.add_task(
+        email_sender.send_password_reset_email,
         str(data.email),
         password_reset_complete_link
     )
@@ -304,8 +313,8 @@ async def request_password_reset_token(
     responses={
         400: {
             "description": (
-                "Bad Request - The provided email or token is invalid, "
-                "the token has expired, or the user account is not active."
+                    "Bad Request - The provided email or token is invalid, "
+                    "the token has expired, or the user account is not active."
             ),
             "content": {
                 "application/json": {
@@ -341,7 +350,8 @@ async def request_password_reset_token(
 async def reset_password(
         data: PasswordResetCompleteRequestSchema,
         db: AsyncSession = Depends(get_db),
-        email_sender: EmailSenderInterface = Depends(get_accounts_email_notificator)
+        email_sender: EmailSenderInterface = Depends(get_accounts_email_notificator),
+        background_tasks: BackgroundTasks = Depends(),
 ) -> MessageResponseSchema:
     """
     Endpoint for resetting a user's password.
@@ -354,6 +364,7 @@ async def reset_password(
          token, and new password.
         db (AsyncSession): The asynchronous database session.
         email_sender (EmailSenderInterface): The asynchronous email sender.
+        background_tasks: BackgroundTasks
 
     Returns:
         MessageResponseSchema: A response message indicating successful password reset.
@@ -407,7 +418,8 @@ async def reset_password(
 
     login_link = "http://127.0.0.1/accounts/login/"
 
-    await email_sender.send_password_reset_complete_email(
+    background_tasks.add_task(
+        email_sender.send_password_reset_complete_email,
         str(data.email),
         login_link
     )
